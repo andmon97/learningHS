4e30dd5345303c0cc52275435e1f2cd32bbe2132
Andrea Montemurro <andrea.montemurro97@gmail.com>
2021-02-13 09:36:17 +0100

    10 diviso 2 e commentiu

--- tests.hs
+++ tests.hs
@@ -1,4 +1,12 @@
+-- Raddoppia x param e quadrupla richiamando double di double di param x --
 double x = x + x
 quadruple x = double (double x)
 
+-- Fattoriale di un numero come prodotto di elementi di una lista contentente da uno a n--
 factorial n = product [1..n]
+
+-- n é uguale a 10 diviso 2 cioé lunghezza di xs--
+n = a `div` length xs
+    where
+        a = 10
+        xs = [1,2,3,4,5]
