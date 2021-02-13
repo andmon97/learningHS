9b7c40187f46db9f3cb7258c8c9846be01e7487b
Andrea Montemurro <andrea.montemurro97@gmail.com>
2021-02-13 11:29:24 +0100

    verifica dei tipi di di queste funzioni con :type

--- tests.hs
+++ tests.hs
@@ -10,3 +10,15 @@ n = a `div` length xs
     where
         a = 10
         xs = [1,2,3,4,5]
+
+-- verifica tipo di queste funzioni--
+-- comando :type--
+second xs     = head (tail xs)
+
+swap (x,y)    = (y,x)
+
+pair x y      = (x,y)
+
+palindrome xs = reverse xs == xs
+
+twice f x     = f (f x) 
