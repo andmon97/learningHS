e3e2207444b4025422ed2943f87aefc17798a4f8
Andrea Montemurro <andrea.montemurro97@gmail.com>
2021-02-14 12:21:55 +0100

    excercise perfect numbers

--- tests.hs
+++ tests.hs
@@ -168,7 +168,15 @@ lowers xs =
 
 -- EXCERCISES
 -- A triple (x,y,z) of positive integers is called pythagorean if x^2 + y^2 = z^2.  Using a list comprehension, define a function that maps an integer n to all such triples with components in [1..n].  
-
 pyths           :: Int -> [(Int, Int, Int)]
 pyths n         =
     [(x, y, z) | x <- [1..n], y <- [1..n], z <- [1..n], x^2 + y^2 == z^2]
+
+-- A positive integer is perfect if it equals the sum of all of its factors, excluding the number itself.  Using a list comprehension, define a function that returns the list of all perfect numbers up to a given limit
+factors'        :: Int -> [Int]
+factors' n      =
+    [x | x <- [1..n-1], n `mod` x == 0]
+
+perfects :: Int -> [Int]
+perfects n = 
+    [x | x <- [1..n], sum (factors' x) == x]
