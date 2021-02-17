7e0a4d0237ca9345ac8f32ee1d71400dc248ec43
Andrea Montemurro <andrea.montemurro97@gmail.com>
2021-02-17 11:19:48 +0100

    foldR and composition functions

--- chapter7.hs
+++ chapter7.hs
@@ -23,3 +23,64 @@ filter'' p (x:xs)
     | otherwise = filter'' p xs
 
 
+
+--      FOLDR PATTERN FUNCTION      --  
+--examples of functions
+
+{- 
+sum []  = 0
+sum (x:xs) = x + sum xs
+
+sum     = foldr (+) 0
+----------------------------------------
+product []     = 1
+product (x:xs) = x * product xs
+
+product = foldr (*) 1
+----------------------------------------
+
+and []     = True
+and (x:xs) = x && and xs
+
+and     = foldr (&&) True
+
+FOLDR associate to right
+-}
+
+length'         :: [a] -> Int
+length'         = foldr (\_ n -> 1+n) 0
+
+reverse'        :: [a] -> [a]
+reverse'        = foldr (\x xs -> xs ++ [x]) [] 
+
+
+
+--          COMPOSITION FUNCTION        --
+-- The library function (.) returns the composition of two functions as a single function. (...) is a re-definition of (.) --
+(...)            :: (b ->c) -> (a -> b) -> (a -> c)
+f ... g          = \x -> f (g x)
+
+-- Example of applications
+odd'            :: Int -> Bool
+odd'            = not ... even
+
+
+
+-- Functions with a predicate --
+-- The library function all decides if every element of a list satisfies a given predicate.all' is a re-definition --
+all'            :: (a -> Bool) -> [a] -> Bool
+all' p xs       = and [p x | x <- xs]
+
+-- Dually, the library function any decides if at least one element of a list satisfies a predicate. any' is a re- definition -- 
+any'            :: (a -> Bool) -> [a] -> Bool
+any' p xs       = or [p x | x <- xs]
+
+-- The library function takeWhile selects elements from a list while a predicate holds of all the elements. --
+takeWhile'      :: (a -> Bool) -> [a] -> [a]
+takeWhile' p [] = []
+takeWhile' p (x:xs)
+        | p x       = x : takeWhile' p xs
+        | otherwise = [] 
+
+
+
